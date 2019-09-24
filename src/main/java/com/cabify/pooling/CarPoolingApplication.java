09583d139d1ac792276fb635c1ce3ff66b294f85
c.friaszapater <c.friaszapater@orwellg.com>
2019-09-24 18:13:15 +0200

    improvement: write concern 'majority' seems not to be needed

--- src/main/java/com/cabify/pooling/CarPoolingApplication.java
+++ src/main/java/com/cabify/pooling/CarPoolingApplication.java
@@ -1,19 +1,10 @@
 package com.cabify.pooling;
 
-import com.mongodb.WriteConcern;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.WriteConcernResolver;
 
 @SpringBootApplication
 public class CarPoolingApplication {
-
-	// TODO possibly not needed
-	@Bean
-	public WriteConcernResolver writeConcernResolver() {
-		return action -> WriteConcern.MAJORITY;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarPoolingApplication.class, args);
